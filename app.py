3811dff2d2f3782215579a5126e0b72617eaf4cb
seyun0122 <seyunkim0122@gmail.com>
2026-05-18 12:04:27 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,16 @@ def main():
 
     port = int(os.environ.get("PORT", 7860))
 
+    print("==== DEBUG START ====")
+    print("PORT =", port)
+    print("APP TYPE =", type(app))
+    print("APP =", app)
+    print("==== DEBUG END ====")
+
     app.launch(
         server_name="0.0.0.0",
-        server_port=port
+        server_port=port,
+        share=False
     )
 
 if __name__ == "__main__":
