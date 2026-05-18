3c9eaf5805ab7accb96bc1f0df530611accf0b62
seyun0122 <seyunkim0122@gmail.com>
2026-05-18 11:56:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from bootstrap.container import Container
+import os
 
 def main():
     app = Container().build_app()
-    app.launch()
+
+    port = int(os.environ.get("PORT", 7860))
+
+    app.launch(
+        server_name="0.0.0.0",
+        server_port=port
+    )
 
 if __name__ == "__main__":
     main()
